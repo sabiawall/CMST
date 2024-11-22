2ca3d0dffb4d87d45c6b4ccf5343730c7fef4d11
unknown <sabi.awal3@gmail.com>
2024-11-22 10:42:36 +0545

    User Seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'sr.paudel09@gmail.com',
+            'password' => Hash::make('Schmaltz@2'),
+        ]);
+    }
+}
